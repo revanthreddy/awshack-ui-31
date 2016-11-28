252348b59987abcdf2bdcb531b304d687a8d89b7
Michael Myers <michael.morris.myers@gmail.com>
2016-11-28 14:10:36 -0800

    hello backend

--- build/js/constants.js
+++ build/js/constants.js
@@ -1,3 +1,7 @@
 module.exports = {
-    DUMMY_PATH: "/dashboard/assets/dummy-data.json"
+    DUMMY_PATH: "/dashboard/assets/dummy-data.json",
+    BASE_URL: "https://mlu6i50zeh.execute-api.us-east-1.amazonaws.com",
+    PATH : {
+        POACHING: "/dev/poaching"
+    }
 }
--- build/js/index.js
+++ build/js/index.js
@@ -13,11 +13,26 @@ var CONSTANTS = require("./constants.js");
                     });
                 }
 
-            }).catch(function(err) {  
+            }).catch(function (err) {
                 console.log('Fetch Error :-S', err);
-                callback(err);  
+                callback(err);
             });
-        }
+        },
+        getPoachingData: function (callback) {
+            var url = CONSTANTS.BASE_URL + CONSTANTS.PATH.POACHING;
+            fetch(url).then(function (response) {
+
+                if (response.status === 200) {
+                    response.json().then(function (data) {
+                        callback(undefined, data);
+                    });
+                }
+
+            }).catch(function (err) {
+                console.log('Fetch Error :-S', err);
+                callback(err);
+            });
+        },
     }
 
     window.api = api;
