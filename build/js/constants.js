82de7351bd3cebf2a28df043756201111263411a
Michael Myers <michael.morris.myers@gmail.com>
2016-11-28 20:37:32 -0800

    adding categories

--- build/js/constants.js
+++ build/js/constants.js
@@ -1,7 +1,27 @@
 module.exports = {
     DUMMY_PATH: "/dashboard/assets/dummy-data.json",
     BASE_URL: "https://mlu6i50zeh.execute-api.us-east-1.amazonaws.com",
-    PATH : {
+    PATH: {
         POACHING: "/dev/poaching"
+    },
+    CATEGORIES: {
+        DISPLAY_NAMES: [
+            "Weapons",
+            "Weapons Seized",
+            "Poacher Encounters",
+            "Wildlife",
+            "People Observed",
+            "Hunting Camp",
+            "Patrol Observation"
+        ],
+        VALUES: [
+            'weapons',
+            'weapons_seized',
+            'poacher_encounters',
+            'wildlife',
+            'people_observed',
+            'hunting_camp',
+            'patrol_observation'
+        ]
     }
 }
